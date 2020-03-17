02f520d27bd8b9a122d10ce8296b8c48c98f41dc
thaisaacastro <thaisascastro@gmail.com>
2020-03-16 21:35:49 -0300

    heranca 2

--- Objetos/Herança02.js
+++ Objetos/Herança02.js
@@ -8,5 +8,37 @@ console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
 
 const carro = {
   velAtual: 0,
-  velMax: 200
+  velMax: 200,
+  acelararMais(delta) {
+    if (this.velAtual + delta <= this.velMax) {
+      this.velAtual += delta
+    } else {
+      this.velAtual = this.velMax
+    }
+  },
+  status() {
+    return `${this.velAtual}Km/h de ${this.velMax}Km/h`
+  }
 }
+const ferrari = {
+  modelo: 'F40',
+  velMax: 324 // shadowing/ sombreamento
+}
+const volvo = {
+  modelo: 'V40',
+  status() {
+    return `${this.modelo}: ${super.status()}`
+  }
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelararMais(100)
+console.log(volvo.status())
+
+ferrari.acelararMais(300)
+console.log(ferrari.status())
