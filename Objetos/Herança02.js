93b1004c7650b5714a00c28ac7b329e0fba9d2fe
thaisaacastro <thaisascastro@gmail.com>
2020-03-17 16:38:50 -0300

    heranca

--- Objetos/Herança02.js
+++ Objetos/Herança02.js
@@ -1,6 +1,6 @@
 // Cadeia de protótipos (prototype chain)
+Object.prototype.attr0 = '0'  // não tenha hábito
 
-Object.prototype.attr0 = '0'
 const avo = { attr1: 'A' }
 const pai = { __proto__: avo, attr2: 'B', attr3: '3' }
 const filho = { __proto__: pai, attr3: 'C' }
