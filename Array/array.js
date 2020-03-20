ba5a7300c544a9c1a00f88eb4c4ecae5648e86d3
thaisaacastro <thaisascastro@gmail.com>
2020-03-20 15:58:04 -0300

    Formas de como usar array

---
+++ Array/array.js
@@ -0,0 +1,31 @@
+console.log(typeof Array, typeof new Array) //new junto de uma função, objeto
+
+let aprovados = new Array('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+aprovados.push('Abia')   //adc novo elemento
+console.log(aprovados.length)
+
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+console.log(aprovados[8] === undefined)
+
+console.log(aprovados)
+aprovados.sort() //ordenar o array
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1, 1) //serve para adc e remover elementos no índices e tb remover elementos no array
+console.log(aprovados)
+
