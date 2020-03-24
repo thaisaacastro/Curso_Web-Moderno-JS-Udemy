21317f4ac292b8357ef3e18fd4c72497baa5427d
thaisaacastro <thaisascastro@gmail.com>
2020-03-24 20:13:44 -0300

    Exportar algo para pasta principal no Modulo

---
+++ Node/MóduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./MóduloA')
+const moduloB = require('./MóduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.boaNoite())
+console.log(moduloB.bomDia)
+console.log(moduloB)
