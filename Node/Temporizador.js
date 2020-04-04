97b9de87b083b6a64f0f9661ef50d18e6d02512d
thaisaacastro <thaisascastro@gmail.com>
2020-04-04 13:39:38 -0300

    Tarefas sendo agendadas com o Temporizaador

--- Node/Temporizador.js
+++ Node/Temporizador.js
@@ -1,7 +1,20 @@
 const schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('*/5 * 12 * * 2', function () {
+const tarefa1 = schedule.scheduleJob('*/5 * 13 * * 2', function () {
   console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
- // 0 = Domingo / 1 = Segunda / 2 = Terça // */5 = 5 em 5 min
+// 0 = Domingo / 1 = Segunda / 2 = Terça // */5 = 5 em 5 min
 
+setTimeout(function () {
+  tarefa1.cancel()
+  console.log('Cancelando Tarefa 1!')
+}, 20000)
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 12
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+  console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
