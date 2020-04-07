6f1641f6353dae3bdac93df0585ef586a4228c77
thaisaacastro <thaisascastro@gmail.com>
2020-04-07 15:54:14 -0300

    Promises e formas de ser usadas

---
+++ ESNext/Promises.js
@@ -0,0 +1,14 @@
+// promises
+function falarDepoisDe(segundos, frase) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(frase) //só um único parâmetrp
+    }, segundos * 1000)
+
+  })
+}
+
+falarDepoisDe(3, 'Que legal!')
+  .then(frase => frase.concat('?!?'))
+  .then(outraFrase => console.log(outraFrase))
+  .catch(e => console.log(e))
