c29e5251bd2afc8aa2b0db69b150cb13b0540cc1
thaisaacastro <thaisascastro@gmail.com>
2020-05-05 16:33:07 -0300

    if no js

--- css/js/tag.js
+++ css/js/tag.js
@@ -2,4 +2,8 @@ document.querySelectorAll('.tag').forEach(box => {
   const tagName = box.tagName.toLowerCase()
 
   box.style.borderColor = '#616161'
+
+  if (!box.classList.contains('nolabel')) {
+    const label = document.createElement('label')
+  }
 })
