6029a3524b744ff4bade66b54cbdcaebd556d444
thaisaacastro <thaisascastro@gmail.com>
2020-07-14 17:47:15 -0300

    implementando o server.js

--- ajax/server.js
+++ ajax/server.js
@@ -5,3 +5,6 @@ const app = express();
 app.use(express.static(".")); // dentro da pasta atual sirva os arq atuais/prove
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // se vier um json vai ser transformado em object
+
+app.get("/teste", (req, res) => res.send("OK"));
+app.listen(8080, () => console.log("Executando..."));
