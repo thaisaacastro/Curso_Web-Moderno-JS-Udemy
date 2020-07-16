a1f21ef0ce08f88b2d460009a64c26c37d309459
thaisaacastro <thaisascastro@gmail.com>
2020-07-16 16:09:37 -0300

    colocando o arquivo upload

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,17 @@ app.use(express.static(".")); // dentro da pasta atual sirva os arq atuais/prove
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // se vier um json vai ser transformado em object
 
-app.get("/teste", (req, res) => res.send("OK"));
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./upload");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`);
+  }, // função chamada
+}); // fazer upload do arq
+
+const upload = multer({ storage }).single("arquivo");
+
 app.listen(8080, () => console.log("Executando..."));
