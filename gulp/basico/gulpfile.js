df55fdb14d5535597785dab0c23911278a68e1d5
thaisaacastro <thaisascastro@gmail.com>
2020-07-30 16:16:39 -0300

    executar tarefas atraves do gulp

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -2,9 +2,24 @@ const gulp = require("gulp"); // importando o gulp
 //const { series } = require("gulp"); //fazer várias tarefas em série
 const series = gulp.series;
 
+const antes1 = (cb) => {
+  console.log("Tarefa Antes 1!");
+  return cb();
+};
+
+const antes2 = (cb) => {
+  console.log("Tarefa Antes 2!");
+  return cb();
+};
+
 function copiar(cb) /*callback*/ {
   console.log("Tarefa de copiar!");
   return cb();
 } // determinar qual tarefa foi concluída para o gulp
 
-module.exports.default = series(copiar);
+const fim = (cb) => {
+  console.log("Tarefa Fim!");
+  return cb();
+};
+
+module.exports.default = series(antes1, antes2, copiar, fim);
