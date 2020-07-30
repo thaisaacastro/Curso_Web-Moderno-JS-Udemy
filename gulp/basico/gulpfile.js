1cd7125e0f81e4aeb0f9b2c434fbc2e39e0fe3fa
thaisaacastro <thaisascastro@gmail.com>
2020-07-30 15:53:20 -0300

    usando a funcao callback para determinar qual tarefa foi finalizada

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,4 +1,8 @@
-const gulp = require("gulp");
-const { series } = require("gulp");
-
+const gulp = require("gulp"); // importando o gulp
+//const { series } = require("gulp"); //fazer várias tarefas em série
 const series = gulp.series;
+
+function copiar(cb) /*callback*/ {
+  console.log("Tarefa de copiar!");
+  return cb();
+} // determinar qual tarefa foi concluída para o gulp
