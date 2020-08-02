7d5a5f1652002b4444e61df8635aca1c3e1c8eae
thaisaacastro <thaisascastro@gmail.com>
2020-08-02 13:28:25 -0300

    realocando

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,22 +1,10 @@
 const gulp = require("gulp"); // importando o gulp
-const { series, parallel } = require("gulp"); //fazer várias tarefas em série
-//const series = gulp.series;
+//const { series, parallel } = require("gulp"); //fazer várias tarefas em série
+const series = gulp.series;
 
-const antes1 = (cb) => {
-  console.log("Tarefa Antes 1!");
-};
-
-function copiar(cb) /*callback*/ {
-  gulp.src(["pastaA.arquivo1.txt", "pastaA.arquivo2.txt"]); //serve para selecionar quais arq vc vai usar como entrada para este det arq
-  // .pipe(); -> serve para fazer transformaçoes ou aplicar nos arq de entrada para ser workflow
-  // .pipe(imagemPelaMetade())
-  // .pipe(imagemEmPretoEBranco())
-  // .pipe(transformacaoA())
-  // .pipe(tranformacaoB())
-  // .pipe(transformacaoC())
-  pipe(gulp.dest("pastaB"));
-
+function copiar(cb) {
+  console.log("Tarefa de copiar!");
   return cb();
-} // determinar qual tarefa foi concluída para o gulp
+}
 
-module.exports.default = series(parallel(antes1, antes2), copiar, fim);
+module.exports.default = series(copiar);
