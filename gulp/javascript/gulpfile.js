8d71ea99e29916c6e43e51c5eb2f89e8e983094f
thaisaacastro <thaisascastro@gmail.com>
2020-08-03 19:24:12 -0300

    importando

---
+++ gulp/javascript/gulpfile.js
@@ -0,0 +1,5 @@
+const { series } = require("gulp");
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
