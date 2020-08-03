acf2dd3b50dfd86de35494cd463042d6084e51e1
thaisaacastro <thaisascastro@gmail.com>
2020-08-03 19:27:26 -0300

    function retornando uma cb

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -3,3 +3,9 @@ const gulp = require("gulp");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const babel = require("gulp-babel");
+
+function padrao(cb) {
+  gulp.src("src/**/*.js"); // qualquer sub pasta que esteja.
+
+  return cb();
+}
