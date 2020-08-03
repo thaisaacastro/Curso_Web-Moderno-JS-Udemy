16e1f3a03167080935e05ce34cfa0a1da1f5a9df
thaisaacastro <thaisascastro@gmail.com>
2020-08-03 19:37:23 -0300

    adicionando formatos de pipe

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -6,6 +6,16 @@ const babel = require("gulp-babel");
 
 function padrao(cb) {
   gulp.src("src/**/*.js"); // qualquer sub pasta que esteja.
+  pipe(
+    babel({
+      comments: false,
+      presets: ["env"], //obj de conf/ pegar a version do JS + novo
+    })
+  );
+
+  pipe(uglify()); // vai pegar todo cod e converter para ser mimificado
+  pipe(concat("codigo.min.js")); // todos os arq selecionados que já foram copilados este res será concatenado
+  pipe(gulp.dest("build")); // destino
 
   return cb();
 }
