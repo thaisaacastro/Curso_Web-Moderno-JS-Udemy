0b442e0ebed0a1e6824c15aeccc99565ce4acff1
thaisaacastro <thaisascastro@gmail.com>
2020-08-04 18:42:32 -0300

    usando o Typescript

--- gulp/typeScript/build/app.js
+++ gulp/typeScript/build/app.js
@@ -4,7 +4,7 @@ var produto_1 = require("./produto");
 function exibir(v) {
     console.log(v.nome + " custa " + v.preco + "!");
 }
-var c = new produto_1.Carro;
-c.nome = 'Civic';
-c.preco = 89499.00;
+var c = new produto_1.Carro();
+c.nome = "Civic";
+c.preco = 89499.0;
 exibir(c);
