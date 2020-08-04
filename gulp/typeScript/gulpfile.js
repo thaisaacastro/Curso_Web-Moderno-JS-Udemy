6998ba3094f49cc65d84362bd7e6b3d81c3a492e
thaisaacastro <thaisascastro@gmail.com>
2020-08-04 18:33:30 -0300

    retornando o projeto

---
+++ gulp/typeScript/gulpfile.js
@@ -0,0 +1,8 @@
+const { series } = require("gulp");
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+
+function transformacaoTS() {
+  return tsProject.src().pipe(tsProject());
+}
