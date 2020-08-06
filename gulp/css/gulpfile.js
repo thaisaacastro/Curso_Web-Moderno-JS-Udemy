7aed9bfe22cd7643433553b24ab5a5c175078fc7
thaisaacastro <thaisascastro@gmail.com>
2020-08-06 16:10:09 -0300

    criando a pasta build

--- gulp/css/gulpfile.js
+++ gulp/css/gulpfile.js
@@ -1,5 +1,16 @@
-const gulp = require("gulp");
 const { series } = require("gulp");
+const gulp = require("gulp");
 const sass = require("gulp-sass");
-const uglifycss = requeire("gulp-unglifycss");
+const uglifycss = require("gulp-uglifycss");
 const concat = require("gulp-concat");
+
+function transformacaoCSS() {
+  return gulp
+    .src("src/sass/index.scss") // font
+    .pipe(sass().on("error", sass.logError)) // passar está function como refer/ converter para CSS
+    .pipe(uglifycss({ uglyComments: true }))
+    .pipe(concat("estilo.min.css"))
+    .pipe(gulp.dest("build/css"));
+}
+
+exports.default = series(transformacaoCSS);
