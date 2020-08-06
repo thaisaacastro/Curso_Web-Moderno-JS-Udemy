07e573a66f097357eda0b10bcd0866e26f20e32a
thaisaacastro <thaisascastro@gmail.com>
2020-08-06 17:43:07 -0300

    gerando a pasta build com as fontes

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,7 +1,13 @@
 const gulp = require("gulp");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
 
-function depsCSS(cb) {
-  return cb();
+function depsCSS() {
+  return gulp
+    .src("node_modules/font-awesome/css/font-awesome.css")
+    .pipe(uglifycss({ uglyComments: false }))
+    .pipe(concat("deps.min.css"))
+    .pipe(gulp.dest("build.assets/css"));
 }
 
 function depsFonts(cb) {
