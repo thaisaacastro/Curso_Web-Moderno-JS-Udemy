4bfe235527db13a028f49fcfd215f6ce19a36eff
thaisaacastro <thaisascastro@gmail.com>
2020-08-25 16:18:53 -0300

    chamando as dezenas

--- jquery/js/temporizador.js
+++ jquery/js/temporizador.js
@@ -10,6 +10,26 @@
     const horaDezena = $('<span class="digito">').html("0");
     const horaUnidade = $('<span class="digito">').html("0");
     const minutoDezena = $('<span class="digito">').html("0");
+    const minutoUnidade = $('<span class="digito">').html("0");
+    const segundoDezena = $('<span class="digito">').html("0");
+    const segundoUnidade = $('<span class="digito">').html("0");
+
+    const separadorHora = $('<span class="separador">').html(":");
+    const separadorMinuto = $('<span class="separador">').html(":");
+    const mensagem = $('<span class="mensagem">').html(opcoesFinais.mensagem);
+
+    $(this).addClass("temporizador");
+    $(this).append(
+      horaDezena,
+      horaUnidade,
+      separadorHora,
+      minutoDezena,
+      minutoUnidade,
+      separadorMinuto,
+      segundoDezena,
+      segundoUnidade,
+      mensagem
+    );
 
     return this;
   };
