4fc4175fe62d0f696efef09efc4836582ec37d4e
thaisaacastro <thaisascastro@gmail.com>
2020-08-25 16:12:11 -0300

    colocando as const

--- jquery/js/temporizador.js
+++ jquery/js/temporizador.js
@@ -3,9 +3,13 @@
     const opcoesFinais = $.extend(
       {
         mensagem: "em breve!",
+        horario: "23:59:59",
       },
       opcoes
     );
+    const horaDezena = $('<span class="digito">').html("0");
+    const horaUnidade = $('<span class="digito">').html("0");
+    const minutoDezena = $('<span class="digito">').html("0");
 
     return this;
   };
